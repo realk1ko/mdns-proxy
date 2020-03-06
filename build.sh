845461cd2a15d5db43e850e99302d4b4b9b98df6
fenik <32820057+fenik@users.noreply.github.com>
2020-03-06 14:51:07 +0100

    Improved readability

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 IMAGE=mdns-proxy
 
-APP_DIR=./app/
+APP_DIR=./app
 
-git clone -b master --single-branch --depth 1 git@github.com:miselin/mDNS-Unicast-Lookup.git $APP_DIR
-git --git-dir="$APP_DIR/.git/" --work-tree=$APP_DIR pull
+git clone -b master --single-branch --depth 1 git@github.com:miselin/mDNS-Unicast-Lookup.git "${APP_DIR}/"
+git --git-dir="${APP_DIR}/.git/" --work-tree="${APP_DIR}/" pull
 
-docker build -t $IMAGE --build-arg APP_DIR=$APP_DIR .
+docker build -t $IMAGE --build-arg APP_DIR="${APP_DIR}/" .
 
